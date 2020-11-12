284edc17ec4a4e7455c8df1dc2760ed1f6701f2e
sotiris-sudo <de@isotiris.com>
2020-11-12 21:15:28 +0100

    changed promise to async/await

---
+++ src/helpers.js
@@ -0,0 +1,18 @@
+// This is taken as is from https://reactjs.org/blog/2015/12/16/ismounted-antipattern.html
+export const makeCancelablePromise = (promise) => {
+  let hasCanceled_ = false;
+
+  const wrappedPromise = new Promise((resolve, reject) => {
+    promise.then(
+      (val) => (hasCanceled_ ? reject({ isCanceled: true }) : resolve(val)),
+      (error) => (hasCanceled_ ? reject({ isCanceled: true }) : reject(error))
+    );
+  });
+
+  return {
+    promise: wrappedPromise,
+    cancel() {
+      hasCanceled_ = true;
+    },
+  };
+};
